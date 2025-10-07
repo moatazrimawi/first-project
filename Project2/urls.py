4d4b92bac533deb9d7039d7bd57f59391c95b7f5
moataz <m-rimawi@hotmail.com>
2025-10-07 19:41:43 +0300

    change the url roor to be an empty root

--- Project2/urls.py
+++ Project2/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('blabla/', include('pages.urls')),
+    path('', include('pages.urls')),
 ]
